fd0761b7d0188d823dfb93956446b5afb255e8a1
Muhammad Almas Rizaldi <almasrzld@gmail.com>
2024-05-08 17:37:12 +0700

    feat: add icons

---
+++ public/icons/index.tsx
@@ -0,0 +1,18 @@
+export default function TopIcon() {
+  return (
+    <svg
+      width="15"
+      height="14"
+      viewBox="0 0 15 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M8.36137 2.66554V13.9335H6.63862V2.66554L1.21799 6.94078L0 5.98016L7.5 0.0649414L15 5.98016L13.782 6.94078L8.36137 2.66554Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
