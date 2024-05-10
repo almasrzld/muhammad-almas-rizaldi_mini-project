04da8d1bcab9175a06fb82eb23e94ae079577946
almasrzld <almasrzld@gmail.com>
2024-05-10 21:35:55 +0700

    feat: enhance footer components

--- features/Base/Home/index.tsx
+++ features/Base/Home/index.tsx
@@ -2,51 +2,8 @@
 
 import React from "react";
 import BlurImage from "@/components/common/image-blur";
-import Image from "next/image";
-import Link from "next/link";
 
 const HomepageFeature = () => {
-  const BIODATA_ITEMS = [
-    {
-      title: "Usefull Links",
-      links: [
-        {
-          name: "Home",
-          path: "/",
-        },
-        {
-          name: "Field",
-          path: "/field",
-        },
-        {
-          name: "About",
-          path: "/about",
-        },
-      ],
-    },
-    {
-      title: "Our Services",
-      links: [
-        {
-          name: "Referee",
-          path: "/",
-        },
-        {
-          name: "Market",
-          path: "/",
-        },
-        {
-          name: "Var",
-          path: "/",
-        },
-        {
-          name: "Tournament",
-          path: "/",
-        },
-      ],
-    },
-  ];
-
   return (
     <main>
       <section className="container h-[800px] grid justify-between grid-cols-12">
@@ -121,74 +78,6 @@ const HomepageFeature = () => {
             eliminates your risk with guaranteed rental payments of 50% of the
             rental price.
           </p>
-        </div>
-      </section>
-      <section className="bg-[#E56E00]/30 rounded-t-[100px]">
-        <div className="container py-16 grid grid-cols-6 gap-10">
-          <div className="col-span-2">
-            <Image
-              src="/images/logo.png"
-              alt="Alcabris Logo"
-              width={140}
-              height={140}
-            />
-            <p className="text-lg mt-2 mb-5">
-              Jl. Slamet Riyadi No. 6 Rembang, Jawa Tengah Indonesia
-            </p>
-            <p className="font-semibold text-lg">
-              Phone:{" "}
-              <a
-                href="tel:+62 812 1519 9600"
-                rel="noreferrer"
-                className="font-normal"
-              >
-                +62 812 1519 9600
-              </a>
-            </p>
-            <p className="font-semibold text-lg">
-              Email:{" "}
-              <a
-                href="mailto:almasrzld@gmail.com"
-                rel="noreferrer"
-                className="font-normal"
-              >
-                almasrzld@gmail.com
-              </a>
-            </p>
-          </div>
-          {BIODATA_ITEMS.map((item, index) => (
-            <div key={index}>
-              <h3 className="font-bold text-2xl">{item.title}</h3>
-              <ul className="mt-3 space-y-3">
-                {item.links.map((link, index) => (
-                  <li key={index}>
-                    <Link
-                      href={link.path as any}
-                      className="text-lg font-medium hover:text-primary transition-colors"
-                    >
-                      {link.name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-          <div className="col-span-2">
-            <h3 className="font-bold text-2xl">Our Social Media</h3>
-            <p className="text-lg mt-3">
-              Cras fermentum odio eu feugiat lide par naso tierra videa magna
-              derita valies
-            </p>
-            <div className="flex items-center gap-5 mt-5">
-              {Array.from({ length: 4 }).map((_, index) => (
-                <a
-                  key={index}
-                  href="#"
-                  className="h-8 w-8 bg-blue-500 rounded-full"
-                ></a>
-              ))}
-            </div>
-          </div>
         </div>
       </section>
     </main>
